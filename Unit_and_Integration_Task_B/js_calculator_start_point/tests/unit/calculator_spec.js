e89916d73ace83a883aeaa0cee1890792240203e
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-11-30 14:21:03 +0000

    first test

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -1,14 +1,22 @@
 var Calculator = require('../../public/js/calculator.js')
 var assert = require('assert')
 
+// Unit Tests:
+
+// - `calculator.add()` - add 1 to 4 and get 5
+// - `calculator.subtract()` subtract 4 from 7 and get 3
+// - `calculator.multiply()` - multiply 3 by 5 and get 15
+// - `calculator.divide()` - divide 21 by 7 and get 3
+
 describe('calculator', function () {
   beforeEach(function () {
     calculator = new Calculator()
   });
 
-  // write unit tests here in the form of "it should do something..."
-  it('it has a sample test', function(){
-    assert.equal(true, true)
+  it('it should add 1 to 4 and get 5', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    assert.strictEqual(calculator.runningTotal, 5);
   })
 
 });
